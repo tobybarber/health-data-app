2e3dba7db499052a8da14b0ea71e3ec0514e4051
Your Name <your.email@example.com>
2025-03-18 12:58:11 +1100

    Minor change

--- app/components/BottomNavigation.tsx
+++ app/components/BottomNavigation.tsx
@@ -13,8 +13,8 @@ export default function BottomNavigation() {
 
   return (
     <>
-      <div className="fixed bottom-[16px] left-0 right-0 h-[1px] bg-gray-800 z-10"></div>
-      <div className="fixed bottom-[10px] left-0 right-0 bg-black/80 backdrop-blur-sm p-3 pt-3 pb-4 flex justify-around items-center z-10">
+      <div className="fixed bottom-[30px] left-0 right-0 h-[1px] bg-gray-800 z-10"></div>
+      <div className="fixed bottom-[24px] left-0 right-0 bg-black/80 backdrop-blur-sm p-3 pt-3 pb-4 flex justify-around items-center z-10">
         <Link
           href="/"
           className="flex flex-col items-center text-center hover:scale-110 transition-all duration-200"
