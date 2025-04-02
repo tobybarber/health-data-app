a48b2705683489f2f159147d4742c6d669c2171e
Your Name <your.email@example.com>
2025-04-02 14:40:52 +1100

    tweaks

--- app/analysis/page.tsx
+++ app/analysis/page.tsx
@@ -786,7 +786,7 @@ export default function Analysis() {
           title="My Analysis"
         >
           {/* Analysis Content Section */}
-          <div className="bg-gray-950/80 backdrop-blur-sm rounded-lg shadow p-6">
+          <div className="bg-gray-950/80 backdrop-blur-sm rounded-lg shadow py-6">
             <div className="flex justify-end mb-3 px-1">
               <div className="flex items-center gap-3">
                 <div className="flex flex-col items-end gap-2">
